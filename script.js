9722acac71c7c0e44c57d33839d2a577b0035672
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-04-09 17:19:34 -0500

    Update style.css and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// tab
+const tabItem = document.querySelectorAll('.tab-header-item');
+const tabHeader = document.querySelector('.tab-header');
+const tabContent = document.querySelectorAll('.tab-content-item');
+
