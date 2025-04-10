fd7aba0bcbcd3c120a3f1a889bfe0d426db55dee
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-04-10 18:25:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const tabItem = document.querySelectorAll('.tab-header-item');
 const tabHeader = document.querySelector('.tab-header');
 const tabContent = document.querySelectorAll('.tab-content-item');
 
+tabHeader.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.tab-header-item');
+
+    if (!clicked) return;
+})
+
+
